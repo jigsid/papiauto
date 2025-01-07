27486db4152e2f5c7ca68e5473c44fd811550599
jigsaw <jigsawsmma@gmail.com>
2025-01-07 11:05:06 +0530

    small fix; create trigger toggle

--- src/components/global/automations/trigger/keywords.tsx
+++ src/components/global/automations/trigger/keywords.tsx
@@ -1,18 +1,19 @@
-import { Input } from '@/components/ui/input'
-import { useKeywords } from '@/hooks/use-automations'
-import { useMutationDataState } from '@/hooks/use-mutation-data'
-import { useQueryAutomation } from '@/hooks/user-queries'
-import { X } from 'lucide-react'
-import React from 'react'
+import { Input } from "@/components/ui/input";
+import { useKeywords } from "@/hooks/use-automations";
+import { useMutationDataState } from "@/hooks/use-mutation-data";
+import { useQueryAutomation } from "@/hooks/user-queries";
+import { X } from "lucide-react";
+import React from "react";
 
 type Props = {
-  id: string
-}
+  id: string;
+};
 
 export const Keywords = ({ id }: Props) => {
-  const { onValueChange, keyword, onKeyPress, deleteMutation } = useKeywords(id)
-  const { latestVariable } = useMutationDataState(['add-keyword'])
-  const { data } = useQueryAutomation(id)
+  const { onValueChange, keyword, onKeyPress, deleteMutation } =
+    useKeywords(id);
+  const { latestVariable } = useMutationDataState(["add-keyword"]);
+  const { data } = useQueryAutomation(id);
 
   return (
     <div className="bg-background-80 flex flex-col gap-y-3 p-3 rounded-xl">
@@ -24,7 +25,8 @@ export const Keywords = ({ id }: Props) => {
           data?.data?.keywords.length > 0 &&
           data?.data?.keywords.map(
             (word) =>
-              word.id !== latestVariable.variables.id && (
+              (!latestVariable?.variables ||
+                word.id !== latestVariable.variables.id) && (
                 <div
                   className="bg-background-90 flex items-center gap-x-2 capitalize text-text-secondary py-1 px-4 rounded-full"
                   key={word.id}
@@ -33,7 +35,7 @@ export const Keywords = ({ id }: Props) => {
                 </div>
               )
           )}
-        {latestVariable && latestVariable.status === 'pending' && (
+        {latestVariable && latestVariable.status === "pending" && (
           <div className="bg-background-90 flex items-center gap-x-2 capitalize text-text-secondary py-1 px-4 rounded-full">
             {latestVariable.variables.keyword}
           </div>
@@ -41,7 +43,7 @@ export const Keywords = ({ id }: Props) => {
         <Input
           placeholder="Add keyword..."
           style={{
-            width: Math.min(Math.max(keyword.length || 10, 2), 50) + 'ch',
+            width: Math.min(Math.max(keyword.length || 10, 2), 50) + "ch",
           }}
           value={keyword}
           className="p-0 bg-transparent ring-0 border-none outline-none"
@@ -50,6 +52,6 @@ export const Keywords = ({ id }: Props) => {
         />
       </div>
     </div>
-  )
-}
-export default Keywords
+  );
+};
+export default Keywords;
