3c248598cb2d78febd22af648d50b44f3cb4fa11
jigsaw <jigsawsmma@gmail.com>
2025-01-10 19:20:35 +0530

    delete button

--- src/components/global/automation-list/index.tsx
+++ src/components/global/automation-list/index.tsx
@@ -1,110 +1,166 @@
-'use client'
-import { usePaths } from '@/hooks/user-nav'
-import { cn, getMonth } from '@/lib/utils'
-import Link from 'next/link'
-import React, { useMemo } from 'react'
-import GradientButton from '../gradient-button'
-import { Button } from '@/components/ui/button'
-import { useQueryAutomations } from '@/hooks/user-queries'
-import CreateAutomation from '../create-automation'
-import { useMutationDataState } from '@/hooks/use-mutation-data'
+"use client";
+import { usePaths } from "@/hooks/user-nav";
+import { cn, getMonth } from "@/lib/utils";
+import Link from "next/link";
+import React, { useMemo, useState } from "react";
+import GradientButton from "../gradient-button";
+import { Button } from "@/components/ui/button";
+import { useQueryAutomations } from "@/hooks/user-queries";
+import CreateAutomation from "../create-automation";
+import { useMutationDataState } from "@/hooks/use-mutation-data";
+import { useDeleteAutomation } from "@/hooks/use-automations";
+import { Trash2 } from "lucide-react";
 
-type Props = {}
+type Keyword = {
+  id: string;
+  word: string;
+  automationId: string | null;
+};
+
+type Automation = {
+  id: string;
+  name: string;
+  createdAt: Date;
+  keywords: Keyword[];
+  listener?: {
+    listener: "SMARTAI" | "MESSAGE";
+  };
+};
+
+type Props = {};
 
 const AutomationList = (props: Props) => {
-  const { data } = useQueryAutomations()
+  const { data } = useQueryAutomations();
+  const { mutate: deleteAutomation, isPending: isDeleting } =
+    useDeleteAutomation();
+  const [deletingIds, setDeletingIds] = useState<string[]>([]);
 
-  const { latestVariable } = useMutationDataState(['create-automation'])
-  console.log(latestVariable)
-  const { pathname } = usePaths()
-  
+  const { latestVariable } = useMutationDataState(["create-automation"]);
+  const { pathname } = usePaths();
+
   const optimisticUiData = useMemo(() => {
-    if ((latestVariable && latestVariable?.variables &&  data)) {
-      const test = [latestVariable.variables, ...data.data]
-      return { data: test }
+    if (latestVariable && latestVariable?.variables && data) {
+      const test = [latestVariable.variables, ...data.data] as Automation[];
+      return {
+        data: test.filter((automation) => !deletingIds.includes(automation.id)),
+      };
     }
-    return data || { data: [] }
-  }, [latestVariable, data])
+    return data
+      ? {
+          data: (data.data as Automation[]).filter(
+            (automation) => !deletingIds.includes(automation.id)
+          ),
+        }
+      : { data: [] };
+  }, [latestVariable, data, deletingIds]);
 
-  if (data?.status !== 200 || data.data.length <= 0) {
+  const handleDelete = (id: string) => {
+    setDeletingIds((prev) => [...prev, id]);
+    deleteAutomation(
+      { id },
+      {
+        onError: () => {
+          // Remove from deletingIds if there's an error
+          setDeletingIds((prev) =>
+            prev.filter((deletingId) => deletingId !== id)
+          );
+        },
+      }
+    );
+  };
+
+  if (!data || data?.status !== 200 || optimisticUiData.data.length <= 0) {
     return (
       <div className="h-[70vh] flex justify-center items-center flex-col gap-y-3">
         <h3 className="text-lg text-gray-400">No Automations </h3>
         <CreateAutomation />
       </div>
-    )
+    );
   }
 
   return (
     <div className="flex flex-col gap-y-3">
-      {optimisticUiData.data!.map((automation) => (
-        <Link
-          href={`${pathname}/${automation.id}`}
+      {optimisticUiData.data.map((automation) => (
+        <div
           key={automation.id}
           className="bg-[#1D1D1D] hover:opacity-80 transition duration-100 rounded-xl p-5 border-[1px] radial--gradient--automations flex border-[#545454]"
         >
-          <div className="flex flex-col flex-1 items-start">
-            <h2 className="text-xl font-semibold">{automation.name}</h2>
-            <p className="text-[#9B9CA0] text-sm font-light mb-2">
-              This is from the comment
-            </p>
+          <Link href={`${pathname}/${automation.id}`} className="flex flex-1">
+            <div className="flex flex-col flex-1 items-start">
+              <h2 className="text-xl font-semibold">{automation.name}</h2>
+              <p className="text-[#9B9CA0] text-sm font-light mb-2">
+                This is from the comment
+              </p>
 
-            {automation.keywords.length > 0 ? (
-              <div className="flex gap-x-2 flex-wrap mt-3">
-                {
-                  //@ts-ignore
-                  automation.keywords.map((keyword, key) => (
+              {automation.keywords.length > 0 ? (
+                <div className="flex gap-x-2 flex-wrap mt-3">
+                  {automation.keywords.map((keyword) => (
                     <div
                       key={keyword.id}
                       className={cn(
-                        'rounded-full px-4 py-1 capitalize',
+                        "rounded-full px-4 py-1 capitalize",
                         (0 + 1) % 1 == 0 &&
-                          'bg-keyword-green/15 border-2 border-keyword-green',
+                          "bg-keyword-green/15 border-2 border-keyword-green",
                         (1 + 1) % 2 == 0 &&
-                          'bg-keyword-purple/15 border-2 border-keyword-purple',
+                          "bg-keyword-purple/15 border-2 border-keyword-purple",
                         (2 + 1) % 3 == 0 &&
-                          'bg-keyword-yellow/15 border-2 border-keyword-yellow',
+                          "bg-keyword-yellow/15 border-2 border-keyword-yellow",
                         (3 + 1) % 4 == 0 &&
-                          'bg-keyword-red/15 border-2 border-keyword-red'
+                          "bg-keyword-red/15 border-2 border-keyword-red"
                       )}
                     >
                       {keyword.word}
                     </div>
-                  ))
-                }
-              </div>
-            ) : (
-              <div className="rounded-full border-2 mt-3 border-dashed border-white/60 px-3 py-1">
-                <p className="text-sm text-[#bfc0c3]">No Keywords</p>
-              </div>
-            )}
-          </div>
+                  ))}
+                </div>
+              ) : (
+                <div className="rounded-full border-2 mt-3 border-dashed border-white/60 px-3 py-1">
+                  <p className="text-sm text-[#bfc0c3]">No Keywords</p>
+                </div>
+              )}
+            </div>
+          </Link>
           <div className="flex flex-col justify-between">
             <p className="capitalize text-sm font-light text-[#9B9CA0]">
-              {getMonth(automation.createdAt.getUTCMonth() + 1)}{' '}
+              {getMonth(automation.createdAt.getUTCMonth() + 1)}{" "}
               {automation.createdAt.getUTCDate() === 1
                 ? `${automation.createdAt.getUTCDate()}st`
-                : `${automation.createdAt.getUTCDate()}th`}{' '}
+                : `${automation.createdAt.getUTCDate()}th`}{" "}
               {automation.createdAt.getUTCFullYear()}
             </p>
 
-            {automation.listener?.listener === 'SMARTAI' ? (
-              <GradientButton
-                type="BUTTON"
-                className="w-full bg-background-80 text-white hover:bg-background-80"
+            <div className="flex flex-col gap-y-2">
+              <Button
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleDelete(automation.id);
+                }}
+                variant="destructive"
+                size="sm"
+                disabled={isDeleting || deletingIds.includes(automation.id)}
+                className="w-full"
               >
-                Smart AI
-              </GradientButton>
-            ) : (
-              <Button className="bg-background-80 hover:bg-background-80 text-white">
-                Standard
+                <Trash2 className="h-4 w-4" />
               </Button>
-            )}
+
+              {automation.listener?.listener === "SMARTAI" ? (
+                <GradientButton
+                  type="BUTTON"
+                  className="w-full bg-background-80 text-white hover:bg-background-80"
+                >
+                  Smart AI
+                </GradientButton>
+              ) : (
+                <Button className="bg-background-80 hover:bg-background-80 text-white">
+                  Standard
+                </Button>
+              )}
+            </div>
           </div>
-        </Link>
+        </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default AutomationList
+export default AutomationList;
--- src/hooks/use-automations.ts
+++ src/hooks/use-automations.ts
@@ -1,44 +1,45 @@
-import { z } from 'zod'
+import { z } from "zod";
 import {
   createAutomations,
+  deleteAutomation,
   deleteKeyword,
   saveKeyword,
   saveListener,
   savePosts,
   saveTrigger,
   updateAutomationName,
-} from '@/actions/automations'
-import { useMutationData } from './use-mutation-data'
-import { useRouter } from 'next/navigation'
-import { useEffect, useRef, useState } from 'react'
-import useZodForm from './use-zod-form'
-import { AppDispatch, useAppSelector } from '@/redux/store'
-import { useDispatch } from 'react-redux'
-import { TRIGGER } from '@/redux/slices/automation'
+} from "@/actions/automations";
+import { useMutationData } from "./use-mutation-data";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
+import useZodForm from "./use-zod-form";
+import { AppDispatch, useAppSelector } from "@/redux/store";
+import { useDispatch } from "react-redux";
+import { TRIGGER } from "@/redux/slices/automation";
 
 export const useCreateAutomation = (id?: string) => {
   const { isPending, mutate } = useMutationData(
-    ['create-automation'],
+    ["create-automation"],
     () => createAutomations(id),
-    'user-automations'
-  )
+    "user-automations"
+  );
 
-  return { isPending, mutate }
-}
+  return { isPending, mutate };
+};
 
 export const useEditAutomation = (automationId: string) => {
-  const [edit, setEdit] = useState(false)
-  const inputRef = useRef<HTMLInputElement | null>(null)
-  const enableEdit = () => setEdit(true)
-  const disableEdit = () => setEdit(false)
+  const [edit, setEdit] = useState(false);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const enableEdit = () => setEdit(true);
+  const disableEdit = () => setEdit(false);
 
   const { isPending, mutate } = useMutationData(
-    ['update-automation'],
+    ["update-automation"],
     (data: { name: string }) =>
       updateAutomationName(automationId, { name: data.name }),
-    'automation-info',
+    "automation-info",
     disableEdit
-  )
+  );
 
   useEffect(() => {
     function handleClickOutside(this: Document, event: MouseEvent) {
@@ -46,18 +47,18 @@ export const useEditAutomation = (automationId: string) => {
         inputRef.current &&
         !inputRef.current.contains(event.target as Node | null)
       ) {
-        if (inputRef.current.value !== '') {
-          mutate({ name: inputRef.current.value })
+        if (inputRef.current.value !== "") {
+          mutate({ name: inputRef.current.value });
         } else {
-          disableEdit()
+          disableEdit();
         }
       }
     }
-    document.addEventListener('mousedown', handleClickOutside)
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [])
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return {
     edit,
@@ -65,109 +66,121 @@ export const useEditAutomation = (automationId: string) => {
     disableEdit,
     inputRef,
     isPending,
-  }
-}
+  };
+};
 
 export const useListener = (id: string) => {
-  const [listener, setListener] = useState<'MESSAGE' | 'SMARTAI' | null>(null)
+  const [listener, setListener] = useState<"MESSAGE" | "SMARTAI" | null>(null);
 
   const promptSchema = z.object({
     prompt: z.string().min(1),
     reply: z.string(),
-  })
+  });
 
   const { isPending, mutate } = useMutationData(
-    ['create-lister'],
+    ["create-lister"],
     (data: { prompt: string; reply: string }) =>
-      saveListener(id, listener || 'MESSAGE', data.prompt, data.reply),
-    'automation-info'
-  )
+      saveListener(id, listener || "MESSAGE", data.prompt, data.reply),
+    "automation-info"
+  );
 
   const { errors, onFormSubmit, register, reset, watch } = useZodForm(
     promptSchema,
     mutate
-  )
+  );
 
-  const onSetListener = (type: 'SMARTAI' | 'MESSAGE') => setListener(type)
-  return { onSetListener, register, onFormSubmit, listener, isPending }
-}
+  const onSetListener = (type: "SMARTAI" | "MESSAGE") => setListener(type);
+  return { onSetListener, register, onFormSubmit, listener, isPending };
+};
 
 export const useTriggers = (id: string) => {
-  const types = useAppSelector((state) => state.AutmationReducer.trigger?.types)
+  const types = useAppSelector(
+    (state) => state.AutmationReducer.trigger?.types
+  );
 
-  const dispatch: AppDispatch = useDispatch()
+  const dispatch: AppDispatch = useDispatch();
 
-  const onSetTrigger = (type: 'COMMENT' | 'DM') =>
-    dispatch(TRIGGER({ trigger: { type } }))
+  const onSetTrigger = (type: "COMMENT" | "DM") =>
+    dispatch(TRIGGER({ trigger: { type } }));
 
   const { isPending, mutate } = useMutationData(
-    ['add-trigger'],
+    ["add-trigger"],
     (data: { types: string[] }) => saveTrigger(id, data.types),
-    'automation-info'
-  )
+    "automation-info"
+  );
 
-  const onSaveTrigger = () => mutate({ types })
-  return { types, onSetTrigger, onSaveTrigger, isPending }
-}
+  const onSaveTrigger = () => mutate({ types });
+  return { types, onSetTrigger, onSaveTrigger, isPending };
+};
 
 export const useKeywords = (id: string) => {
-  const [keyword, setKeyword] = useState('')
+  const [keyword, setKeyword] = useState("");
   const onValueChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setKeyword(e.target.value)
+    setKeyword(e.target.value);
 
   const { mutate } = useMutationData(
-    ['add-keyword'],
+    ["add-keyword"],
     (data: { keyword: string }) => saveKeyword(id, data.keyword),
-    'automation-info',
-    () => setKeyword('')
-  )
+    "automation-info",
+    () => setKeyword("")
+  );
 
   const onKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      mutate({ keyword })
-      setKeyword('')
+    if (e.key === "Enter") {
+      mutate({ keyword });
+      setKeyword("");
     }
-  }
+  };
 
   const { mutate: deleteMutation } = useMutationData(
-    ['delete-keyword'],
+    ["delete-keyword"],
     (data: { id: string }) => deleteKeyword(data.id),
-    'automation-info'
-  )
+    "automation-info"
+  );
 
-  return { keyword, onValueChange, onKeyPress, deleteMutation }
-}
+  return { keyword, onValueChange, onKeyPress, deleteMutation };
+};
 
 export const useAutomationPosts = (id: string) => {
   const [posts, setPosts] = useState<
     {
-      postid: string
-      caption?: string
-      media: string
-      mediaType: 'IMAGE' | 'VIDEO' | 'CAROSEL_ALBUM'
+      postid: string;
+      caption?: string;
+      media: string;
+      mediaType: "IMAGE" | "VIDEO" | "CAROSEL_ALBUM";
     }[]
-  >([])
+  >([]);
 
   const onSelectPost = (post: {
-    postid: string
-    caption?: string
-    media: string
-    mediaType: 'IMAGE' | 'VIDEO' | 'CAROSEL_ALBUM'
+    postid: string;
+    caption?: string;
+    media: string;
+    mediaType: "IMAGE" | "VIDEO" | "CAROSEL_ALBUM";
   }) => {
     setPosts((prevItems) => {
       if (prevItems.find((p) => p.postid === post.postid)) {
-        return prevItems.filter((item) => item.postid !== post.postid)
+        return prevItems.filter((item) => item.postid !== post.postid);
       } else {
-        return [...prevItems, post]
+        return [...prevItems, post];
       }
-    })
-  }
+    });
+  };
 
   const { mutate, isPending } = useMutationData(
-    ['attach-posts'],
+    ["attach-posts"],
     () => savePosts(id, posts),
-    'automation-info',
+    "automation-info",
     () => setPosts([])
-  )
-  return { posts, onSelectPost, mutate, isPending }
-}
+  );
+  return { posts, onSelectPost, mutate, isPending };
+};
+
+export const useDeleteAutomation = () => {
+  const { mutate, isPending } = useMutationData(
+    ["delete-automation"],
+    (data: { id: string }) => deleteAutomation(data.id),
+    "user-automations"
+  );
+
+  return { mutate, isPending };
+};
