9619c87ed11d03988caddeff117f60e7b301460d
jigsaw <jigsawsmma@gmail.com>
2025-02-20 21:18:38 +0530

    o

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,6 +10,7 @@ export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
 })
 
+
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
