5c4aff4cc3d215c2d17e0d1d0d93711759795433
jigsaw <jigsawsmma@gmail.com>
2025-03-05 18:08:06 +0530

    FINAL.PAIN.I HAVE GIVEN UP TO MUCH TO STOP NOW

--- src/constants/integrations.tsx
+++ src/constants/integrations.tsx
@@ -11,7 +11,7 @@ export const INTEGRATION_CARDS: Props[] = [
   {
     title: 'Connect Instagram',
     description:
-      'Lorem ipsum dolor sit amet consectetur. Mauris scelerisque tincidunt ultrices',
+      'Connect your Instagram Business account to automate messages and responses',
     icon: <InstagramDuoToneBlue />,
     strategy: 'INSTAGRAM',
     
@@ -19,7 +19,7 @@ export const INTEGRATION_CARDS: Props[] = [
   {
     title: 'Connect Salesforce',
     description:
-      'Lorem ipsum dolor sit amet consectetur. Mauris scelerisque tincidunt ultrices',
+      'Connect your SalesForce Business account to automate messages and responses',
     icon: <SalesForceDuoToneBlue />,
     strategy: 'CRM',
   },
